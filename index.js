c4ca215604e5cdb8e0b5fcb6a45e7bdc99ad5f78
VT-archive <88647756+VT-archive@users.noreply.github.com>
2021-08-09 16:43:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+var puller;
+var active;
+
+//writes object array to screen
+function write1(given){
+  let writer = '';
+
+  given.forEach(value => {
+    writer += "<a target = '_blank' href = " + value.link + ">" + value.name + ":</a>" + ' ' + value.tags + "<br>";
+  });
+
+  document.getElementById("listing").innerHTML = writer;
+};
+
+//fetches json file, hands data to var puller, and writes listing to screen 
+//this thing is a massive pain in the rear to work with, good luck to any who try
+fetch("./library.json")
+  .then(
+    function(response) {
+      response.json().then((data) => { 
+          puller = data;
+          write1(puller);
+          active = puller;
+      });
+    }
+  );
+//ocd
+
+//flips order of listing and writes to screen
+document.getElementById("flip").addEventListener("click", function(){
+  let t = []
+  for(let i = active.length; i != 0; i--){
+    t.push(active[i-1])
+  };
+  active = t;
+  write1(active);
+});
+
+//handles searches
+document.getElementById("search").addEventListener("click", function(){
+  let given = document.getElementById('tag').value.toLowerCase();
+  let giver = [];
+  if(given != ""){
+    puller.forEach(value =>{
+      let worker = value.tags;
+      let stat =0;
+      worker.forEach(value =>{
+        let compare = value.toLowerCase();
+        if ( compare.indexOf(given) > -1 ) {
+          stat++;
+        } 
+      });
+      if(stat > 0){
+        giver.push(value)
+      };
+    });
+
+    if(giver.length != 0){
+      active = giver
+      write1(active);
+    }
+  };
+});
+
+//handles and redirects enter key actions 
+document.getElementById('tag').addEventListener("keypress",function(event){
+  if (event.keyCode === 13){
+    if(document.getElementById('tag').value == ''){
+      document.getElementById("reset").click();
+    }else{
+      document.getElementById("search").click();
+    }
+  }
+});
+
+//resets listing to original
+document.getElementById("reset").addEventListener("click", function(){
+  active = puller;
+  write1(active);
+});
